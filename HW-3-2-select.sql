0224b82f71c1e13ab25dabed528de7de3a35a52a
clot-of-stone <m.ahmanaev@gmail.com>
2022-08-30 23:58:47 +0500

    HW-3 completed (and beautified)

--- HW-3-2-select.sql
+++ HW-3-2-select.sql
@@ -1,18 +1,47 @@
 -- поскольку в моей базе данных отсутствуют альбомы, вышедшие в 2018 году, я взял на себя смелость сделать запрос, где искомый год 2010
-SELECT title, release_year FROM albums 
-WHERE release_year = 2010;
-
-SELECT track_name, run FROM tracks 
-WHERE run = (SELECT MAX(run) FROM tracks);
-
-SELECT track_name, run FROM tracks 
-WHERE run > '00:03:30';
-
-SELECT title, release_year FROM songsters 
-WHERE release_year BETWEEN 2018 AND 2020;
-
-SELECT nickname FROM performers 
-WHERE nickname NOT LIKE '%% %%';
-
-SELECT track_name FROM tracks 
-WHERE track_name ~~* '%% my %%';
+SELECT
+  title,
+  release_year
+FROM
+  albums
+WHERE
+  release_year = 2010;
+SELECT
+  track_name,
+  run
+FROM
+  tracks
+WHERE
+  run = (
+    SELECT
+      MAX(run)
+    FROM
+      tracks
+  );
+SELECT
+  track_name,
+  run
+FROM
+  tracks
+WHERE
+  run > '00:03:30';
+SELECT
+  title,
+  release_year
+FROM
+  songsters
+WHERE
+  release_year BETWEEN 2018
+  AND 2020;
+SELECT
+  nickname
+FROM
+  performers
+WHERE
+  nickname NOT LIKE '%% %%';
+SELECT
+  track_name
+FROM
+  tracks
+WHERE
+  track_name ~~* '%% my %%';
